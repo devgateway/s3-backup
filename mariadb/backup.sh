85f0d6972cfb4a1c7b8cb3184e1def094ad2034b
Styopa Semenukha <ssemenukha@developmentgateway.org>
2019-04-08 16:44:50 -0400

    Add full backup

--- mariadb/backup.sh
+++ mariadb/backup.sh
@@ -2,6 +2,25 @@
 # MariaDB backup/restore helper
 # Copyright 2019, Development Gateway, GPL3+
 
+: ${BACKUP_ROOT:=/var/spool/backup}
+
+create_backup_dir() {
+    local DIR="$BACKUP_ROOT/$(date +%Y-%m-%d_%T)_$1"
+
+    mkdir "$DIR"
+    echo "$DIR"
+}
+
+run_backup_full() {
+    local FULL_DIR
+
+    find "$BACKUP_ROOT" -mindepth 1 -delete
+    FULL_DIR="$(create_backup_dir full)"
+    mariabackup \
+        --backup \
+        --target-dir="$FULL_DIR"
+}
+
 case "$1" in
     full)
         run_backup_full
