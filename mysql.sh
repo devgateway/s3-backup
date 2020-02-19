6bbd5d9ed0b14acd1688aefc589a8f16ee1c2be1
Styopa Semenukha <ssemenukha@developmentgateway.org>
2020-02-18 19:23:25 -0500

    Add Mysql backup script

---
+++ mysql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source functions.sh
+
+if ! check_vars S3_BUCKET_NAME S3_PREFIX 1; then
+  exit 1
+fi
+
+SUFFIX=.sql.gz
+DUMP_FILE="$(umask 077; mktemp --suffix=$SUFFIX)"
+mysqldump "$1" | gzip > "$DUMP_FILE"
+
+S3_SUBDIR="$(basename "$1" | s3_escape)"
+S3_PATH="s3://$S3_BUCKET_NAME/$S3_PREFIX/$S3_SUBDIR/$(date +%F)$SUFFIX"
+aws s3 cp "$DUMP_FILE" "$S3_PATH"
+RC=$?
+rm -f "$DUMP_FILE"
+exit $RC
