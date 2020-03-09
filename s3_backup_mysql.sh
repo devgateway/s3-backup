7d99943b57df4e1394cf0e84fb4c0291c1538fd9
Styopa Semenukha <ssemenukha@developmentgateway.org>
2020-03-09 11:13:31 -0400

    Handle errors

--- s3_backup_mysql.sh
+++ s3_backup_mysql.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 source functions.sh
 
 if ! check_vars S3_BUCKET_NAME S3_PREFIX 1; then
@@ -9,9 +10,11 @@ SUFFIX=.sql.gz
 DUMP_FILE="$(umask 077; mktemp --suffix=$SUFFIX)"
 mysqldump "$1" | gzip > "$DUMP_FILE"
 
-S3_SUBDIR="$(basename "$1" | s3_escape)"
+S3_SUBDIR="$(echo "$1" | s3_escape)"
 S3_PATH="s3://$S3_BUCKET_NAME/$S3_PREFIX/$S3_SUBDIR/$(date +%F)$SUFFIX"
+set +e
 aws s3 cp "$DUMP_FILE" "$S3_PATH"
 RC=$?
+set -e
 rm -f "$DUMP_FILE"
 exit $RC
