27cf0fc5e1c04b881f1eea76861028dff5c54379
Styopa Semenukha <ssemenukha@developmentgateway.org>
2020-03-10 13:04:45 -0400

    Upload to S3 quietly

--- s3_backup_mysql.sh
+++ s3_backup_mysql.sh
@@ -13,7 +13,7 @@ mysqldump "$1" | gzip > "$DUMP_FILE"
 S3_SUBDIR="$(echo "$1" | s3_escape)"
 S3_PATH="s3://$S3_BUCKET_NAME/$S3_PREFIX/$S3_SUBDIR/$(date +%F)$SUFFIX"
 set +e
-aws s3 cp "$DUMP_FILE" "$S3_PATH"
+aws s3 cp "$DUMP_FILE" "$S3_PATH" --quiet
 RC=$?
 set -e
 rm -f "$DUMP_FILE"
