0ac975ba8967327f52e3c3bff3a751ea9f669350
Styopa Semenukha <ssemenukha@developmentgateway.org>
2020-06-15 12:02:44 -0400

    Draw the rest of the Postgres owl

---
+++ s3_backup_postgres.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+. functions.sh
+
+if ! check_vars S3_BUCKET_NAME S3_PREFIX 1; then
+  exit 1
+fi
+
+SUFFIX=.sql.gz
+DUMP_FILE="$(umask 077; mktemp --suffix=$SUFFIX)"
+pg_dump "$1" | gzip > "$DUMP_FILE"
+
+S3_SUBDIR="$(echo "$1" | s3_escape)"
+S3_PATH="s3://$S3_BUCKET_NAME/$S3_PREFIX/$S3_SUBDIR/$(date +%F)$SUFFIX"
+set +e
+aws s3 cp "$DUMP_FILE" "$S3_PATH" --quiet
+RC=$?
+set -e
+rm -f "$DUMP_FILE"
+exit $RC
