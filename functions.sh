90486b81a3b816dab3891132b271e6b4908ce931
Styopa Semenukha <ssemenukha@developmentgateway.org>
2021-01-29 13:26:46 -0500

    Extract reusable functions from PG

--- functions.sh
+++ functions.sh
@@ -1,13 +1,39 @@
+TAR_BLOCKING_FACTOR=20
+
+exit_with_error() {
+  local RET=$1
+  shift
+  echo "$@" >&2
+  exit $RET
+}
+
 check_vars() {
   for VAR_NAME in $@; do
     if eval "test -z \"\$$VAR_NAME\""; then
-      echo "$VAR_NAME not defined" >&2
-      echo "Required variables are: $@" >&2
-      return 1
+      exit_with_error 1 "$VAR_NAME not defined. Required variables are: $@"
     fi
   done
 }
 
 s3_escape() {
   tr -c "[:alnum:]-_.*'()!\\n" _
+}
+
+estimate_size() {
+  # ustar format uses 512 B blocks
+  local BLOCK_SIZE=512
+  local APPARENT_BLOCKS="$(du --apparent-size -sB $BLOCK_SIZE "$1" | grep -o '^[[:digit:]]\+')"
+
+  # each entry has a one-block header
+  local HEADER_BLOCKS="$(find "$1" -mindepth 1 -printf 1 | wc -c)"
+
+  # two zero blocks to mark EOF
+  local BLOCKS=$(($APPARENT_BLOCKS + $HEADER_BLOCKS + 2))
+
+  # round the blocks up to TAR_BLOCKING_FACTOR
+  if [ $(($BLOCKS % $TAR_BLOCKING_FACTOR)) -ne 0 ]; then
+    BLOCKS="$((($BLOCKS / $TAR_BLOCKING_FACTOR + 1) * $TAR_BLOCKING_FACTOR))"
+  fi
+
+  echo "$(($BLOCKS * $BLOCK_SIZE))"
 }
--- postgres/s3_backup_postgres.sh
+++ postgres/s3_backup_postgres.sh
@@ -1,20 +1,37 @@
-#!/bin/sh
-set -e
+#!/bin/sh -e
+# Back up Postgres to S3 bucket
+# Usage: $0 base|wal
+
 . functions.sh
 
-if ! check_vars S3_BUCKET_NAME S3_PREFIX 1; then
-  exit 1
+check_vars S3_BUCKET_NAME
+
+do_base_backup() {
+  pg_basebackup --wal-method=none --format=tar --pgdata=-
+}
+
+do_wal_backup() {
+  tar -b $TAR_BLOCKING_FACTOR -cC "$1" --warning=no-file-changed --warning=no-file-removed .
+}
+
+s3_upload_stdin() {
+  local S3_URL="s3://$S3_BUCKET_NAME/$S3_PREFIX$(date "+$S3_FILE_NAME" | s3_escape)"
+  aws s3 cp - "$S3_URL" --expected-size "$1" --quiet
+}
+
+: ${S3_FILE_NAME:=%F_%T.tar.gz}
+
+if [ "$1" = "base" ]; then
+  echo "Doing base backup"
+  DATA_DIR="$(psql -Atc "SELECT setting FROM pg_settings WHERE name = 'data_directory'")"
+  SIZE="$(estimate_size "$DATA_DIR")"
+  do_base_backup | gzip | s3_upload_stdin "$SIZE"
+else
+  check_vars WAL_DIR
+  echo "Doing WAL backup from $WAL_DIR"
+  SIZE="$(estimate_size "$WAL_DIR")"
+  TIMESTAMP="$(mktemp)"
+  trap "rm -f '$TIMESTAMP'" EXIT
+  do_wal_backup "$WAL_DIR" | gzip | s3_upload_stdin "$SIZE"
+  find "$WAL_DIR" -type f ! -newer "$TIMESTAMP" -delete
 fi
-
-SUFFIX=.sql.gz
-DUMP_FILE="$(umask 077; mktemp --suffix=$SUFFIX)"
-su -c "pg_dump $1" postgres | gzip > "$DUMP_FILE"
-
-S3_SUBDIR="$(echo "$1" | s3_escape)"
-S3_PATH="s3://$S3_BUCKET_NAME/$S3_PREFIX/$S3_SUBDIR/$(date +%F_%R)$SUFFIX"
-set +e
-aws s3 cp "$DUMP_FILE" "$S3_PATH" --quiet
-RC=$?
-set -e
-rm -f "$DUMP_FILE"
-exit $RC
